a09df255c0a8da97b9d85ecbcef3c50c9731e40d
DeepCoreB4 <icarus.mod56@gmail.com>
2025-03-11 08:50:09 +0100

    Fix: Gradle-Konfiguration und Navigation-Dependencies aktualisiert

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -88,7 +88,6 @@ android {
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
-            signingConfig = signingConfigs.getByName("release")
         }
     }
     compileOptions {
