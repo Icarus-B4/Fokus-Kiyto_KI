45d0dce083edbc2f005b8a4e89e8c7fe74878bea
DeepCoreB4 <icarus.mod56@gmail.com>
2025-03-14 23:53:40 +0100

    Release v2.0.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ android {
         applicationId = "com.deepcore.kiytoapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 20008
+        versionCode = 20009
         versionName = "2.0.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -167,6 +167,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 }
+
 
 
 
