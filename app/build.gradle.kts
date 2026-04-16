6356629bc87ef1f2cefec00ad325a7084c559db6
DeepCoreB4 <icarus.mod56@gmail.com>
2026-04-16 21:50:04 +0200

    HOTFIX: Pipeline-Konflikt behoben. v2.1.8 mit robustem Versions-Parsing in GitHub Actions.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,8 +46,8 @@ android {
         applicationId = "com.deepcore.kiytoapp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 20107
-        versionName = "2.1.7"
+        versionCode = 20108
+        versionName = "2.1.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
@@ -164,6 +164,7 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 }
+
 
 
 
